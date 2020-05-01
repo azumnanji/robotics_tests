991693f3bfe2967498887c737cad4fbcfdef2bee
Azum Nanji <a8nanji@uwaterloo.com>
2020-05-01 17:05:18 -0400

    1st test node for odometry

--- odometry/src/odom.cpp
+++ odometry/src/odom.cpp
@@ -17,10 +17,10 @@ struct enc{
     }
 };
 
-class Position{
+class Odom{
     public:
         
-        Position(ros::NodeHandle &nh, unsigned int max_ticks, unsigned int min_ticks, double base_width, 
+        Odom(ros::NodeHandle &nh, unsigned int max_ticks, unsigned int min_ticks, double base_width, 
                  int freq, unsigned int ticks_per_meter) {
             _heading = 0;
             _x = 0;
@@ -41,11 +41,12 @@ class Position{
 
         void update(const std_msgs::Int64 & right_tick, const std_msgs::Int64 & left_tick) {
             curr_time = ros::Time::now().toSec();
+            double time = curr_time - prev_time;
             right.curr_tick = right_tick.data;
             left.curr_tick = left_tick.data;
 
             for (int i = 0; i < 2; ++i) {
-                enc temp = i == 0 ? right:left;
+                enc & temp = i == 0 ? right:left;
                 update_encoder(temp);
             }
 
@@ -55,6 +56,19 @@ class Position{
             double heading_inc = (dist_right - dist_left) / _base_width;
             double x_inc = cos(heading_inc) * (dist_left + dist_right) / 2;
             double y_inc = sin(heading_inc) * (dist_left + dist_right) / 2;
+
+            update_heading(heading_inc);
+
+            double x_fixed = cos(_heading) * x_inc - sin(_heading) * y_inc;
+            double y_fixed = sin(_heading) * x_inc + cos(_heading) * y_inc;
+            _x += x_fixed;
+            _y += y_fixed;
+
+            _x_speed = x_fixed / time;
+            _y_speed = y_fixed / time;
+            _rot_speed = heading_inc / time;
+
+            publish_odom();
 
             // need instantaneous speed and instantaneous rotational speed
             prev_time = curr_time;
@@ -69,22 +83,69 @@ class Position{
                 temp.curr_tick = temp.curr_tick - temp.prev_tick;
         }
 
+        void update_heading(double heading_inc) {
+            _heading += heading_inc;
+            if (_heading > 180)
+                _heading = _heading - 360;
+            else if (_heading < -180)
+                _heading = _heading + 360;
+        }
+
+        void publish_odom() {
+            nav_msgs::Odometry msg;
+            geometry_msgs::Quaternion quat_msg;
+            tf2::Quaternion q;
+            q.setRPY(0, 0, _heading);
+
+            msg.pose.pose.position.x = _x;
+            msg.pose.pose.position.y = _y;
+            msg.pose.pose.position.z = 0;
+            msg.pose.pose.orientation = tf2::convert(quat_msg , q);
+
+            msg.twist.twist.linear.x = _x_speed;
+            msg.twist.twist.linear.y = _y_speed;
+            msg.twist.twist.linear.z = 0;
+            msg.twist.twist.angular.x = 0;
+            msg.twist.twist.angular.y = 0;
+            msg.twist.twist.angular.z = _rot_speed;
+
+            odometry.publish(msg);
+        }
+
+        message_filters::Subscriber<std_msgs::Int64> enc_right;
+        message_filters::Subscriber<std_msgs::Int64> enc_left;
+        ros::Publisher odometry;
+
     protected:
         enc right, left;
         double _heading;
-        double _x;
-        double _y;
+        double _x, _y;
+        double _x_speed, _y_speed;
+        double _rot_speed;
         static unsigned int _max_ticks;
         static unsigned int _min_ticks;
         static unsigned int _ticks_per_meter;
         static double _base_width;
-        message_filters::Subscriber<std_msgs::Int64> enc_right;
-        message_filters::Subscriber<std_msgs::Int64> enc_left;
-        ros::Publisher odometry;
-        ros::Time prev_time, curr_time;
+        double prev_time, curr_time;
 };
 
 
 int main(int argc, char** argv) {
-    return 0;
+    ros::init(argc, argv, "odom");
+
+    ros::NodeHandle n;
+
+    double base_width;
+    int ticks_per_m, freq, min_ticks, max_ticks;
+
+    n.getParam("base_width", base_width);
+    n.getParam("ticks_per_meter", ticks_per_m);
+    n.getParam("fixed_frequency", freq);
+    n.getParam("min_ticks", min_ticks);
+    n.getParam("max_ticks", max_ticks);
+
+    Odom odom(n, max_ticks, min_ticks, base_width, freq, ticks_per_m);
+
+    ros::spin();
+
 }
